82d7bff81de3c8396b545bca69b04f8c3fbb3ed3
Andrew TOwnsley <atownsley86@gmail.com>
2022-02-17 21:38:24 -0700

    route file is running and returning posts.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import fetch from 'node-fetch';
+import postRoutes from './routes/posts.js' 
 
 const app = express();
 
@@ -8,21 +8,6 @@ app.use(bodyParser.json());
 
 const PORT = 5000;
 
-const checkResponseStatus = (res) => {
-    if(res.ok) {
-        return res
-    } else {
-        throw new Error(`The HTTP Status of the response: ${res.status} (${res.statusText})`)
-    }
-}
-
-
-app.get('/', (req, res) => {
-    fetch(`https://api.hatchways.io/assessment/blog/posts?tag=tech`)
-        .then(checkResponseStatus)
-        .then(res => res.json())
-        .then(json => res.send(json.posts[0]))
-        .catch(err => console.log(err))
-});
+app.use('/posts', postRoutes);
 // app.get('/', (req, res) => res.json(`Home Page`);
 app.listen(PORT, () => console.log(`Server is listening on Port: ${PORT}`));
--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,22 @@
 import express from 'express';
+import fetch from 'node-fetch';
 
-const router = express.router();
+const router = express.Router();
 
+const checkResponseStatus = (res) => {
+    if(res.ok) {
+        return res
+    } else {
+        throw new Error(`The HTTP Status of the response: ${res.status} (${res.statusText})`)
+    }
+}
+
+router.get('/', (req, res) => {
+    fetch(`https://api.hatchways.io/assessment/blog/posts?tag=tech`)
+    .then(checkResponseStatus)
+    .then(res => res.json())
+    .then(json => res.send(json.posts))
+    .catch(err => console.log(err))
+})
+
+export default router;
